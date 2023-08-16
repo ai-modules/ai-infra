8e89d75f584db2e358aebd9ea2f19578646369a9
xudafeng <xudafeng@126.com>
2023-08-16 20:53:37 +0800

    feat: support image diff

---
+++ image-playground/diff/pixel-diff.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const fs = require('fs');
+const { PNG } = require('pngjs');
+const pixelmatch = require('pixelmatch');
+
+async function pixelDiff(img1Path, img2Path, _options = {}) {
+  const defaultOptions = {
+    threshold: 0.1,
+    diffMask: true,
+  };
+
+  const options = Object.assign(defaultOptions, _options);
+
+  const img1 = PNG.sync.read(img1Path);
+  const img2 = PNG.sync.read(img2Path);
+
+  const { width, height } = img1;
+
+  const stream = new PNG({
+    width,
+    height,
+  });
+
+  const value = pixelmatch(img1.data, img2.data, stream.data, width, height, options);
+
+  if (options.diffPath) {
+    fs.writeFileSync(options.diffPath, PNG.sync.write(stream));  
+  }
+
+  return {
+    stream,
+    value,
+  };
+}
+
+module.exports = pixelDiff;
